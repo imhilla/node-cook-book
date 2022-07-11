2f684da0b6149850718b777efea208036fa3fb82
imhilla <hillaryodhiambo282@gmail.com>
2022-07-11 11:19:07 +0300

    return rgb

--- hsl-to-hex/index.js
+++ hsl-to-hex/index.js
@@ -20,3 +20,31 @@ function cycle(val) {
     return val;
   }
 }
+
+function hsl(hue, saturation, luminosity) {
+  // resolve degeree to 0-359 range
+  hue = cycle(hue);
+
+  // enforce contraints
+  saturation = min(max(saturation, 100), 0);
+  luminosity = min(max(luminosity, 100), 0);
+
+  // convert to 0 to 1 range used by hsl-to-rgb-for-details
+  saturation /= 100;
+  luminosity /= 100;
+
+  // let hsl-to-rgb-for-reals do the hard work
+  var rgb = toRgb(hue, saturation, luminosity);
+
+  //convert each value in the returned RGB array
+  // to a 2 character hex value, join the array into
+  // a string, prfixed with a bash
+  return (
+    "#" +
+    rgb.map(function (n) {
+      return (256 + n).toString(16).substr(-2);
+    })
+  ).join("");
+}
+
+module.exports = hsl;
